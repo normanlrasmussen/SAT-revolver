c9c5f822a4729b47b8c79eec3cee625b87a2ff2b
Norman <normansawesome@gmail.com>
2025-07-12 15:32:54 -0600

    DPLL almost finished

--- DPLL.py
+++ DPLL.py
@@ -4,35 +4,148 @@
 # TODO Unit propagation - IF there is only choce for a clause, do it
 # TODO Pure Literal Elimination - Elimiating clauses that have no effect on the rest of the system
 
-def DPLL(clauses:list) -> bool:
-    """
-    Perfroms the DPLL algorithm on a list of clauses
-    returns True is satisfiable, False if otherwise
-    """
+class DPLL:
+    def __init__(self, clauses:list[list]):
+        if not clauses:
+            raise ValueError("No Clauses given")
+        
+        self.clauses = clauses
+        self.solved = False
 
-    # Unit progpagation
-    while any(len(clause) == 1 for clause in clauses):
-        for i in range(len(clauses)):
-            if range(len(clauses[i])) == 1:
-                pass
-
-    # Pure Literal Elimination
-    for pure_literal in clauses:
-        # TODO implement pure literal elimation
-        pass
-
-    # Stopping Conditions
-    if not clauses:
-        # If clauses is empty return True
-        return True
+    # -------------------Helper Functions----------------------------
+    def update_clauses(self, clauses:list, literal:int):
+        """
+        Takes in clauses and literal and removes litereal from clauses
+        """
+        
+        new_clauses = []
+        for clause in clauses:
+            if literal in clause:
+                # Clause is satisfied, so remove it
+                continue
+            elif -literal in clause:
+                # Remove the negated literal from clause
+                new_clause = [l for l in clause if l != -literal]
+                if not new_clause:
+                    # Conflict: empty clause created
+                    return None
+                new_clauses.append(new_clause)
+            else:
+                # Clause unchanged
+                new_clauses.append(clause)
+        return new_clauses
     
-    if False:
-        # if there is an empty clause
-        return False
+    def unit_propagate(self, clauses:list, assignment:dict):
+        """
+        Perform unit propagation on clauses (list of lists).
+        Returns (updated_clauses, updated_assignment).
+        """
+
+        changed = True
+        while changed:
+            changed = False
+            # Find all unit clauses
+            unit_clauses = [c[0] for c in clauses if len(c) == 1]
+            if not unit_clauses:
+                break
+            # Pick one unit clause to propagate
+            unit = unit_clauses[0]
+            assignment[abs(unit)] = unit > 0
+
+            clauses = self.update_clauses(clauses, unit)
+            if clauses is None:
+                return None, None
+            changed = True  # Since we modified the formula, check for more unit clauses
+
+        return clauses, assignment
+
+    def pure_literal_elimination(self, clauses:list, assignment:dict):
+        """
+        Perform pure literal elimination on clauses (list of lists).
+        Returns (updated_clauses, updated_assignment).
+        """
+
+        changed = True
+        while changed:
+            changed = False
+            # Find all pure litereals
+            all_literals = set([literal for clause in clauses for literal in clause])
+            
+            pure_literal = None
+            for literal in all_literals:
+                if -literal not in all_literals:
+                    pure_literal = literal
+                    break
+            
+            if pure_literal is None:
+                break
+            
+            assignment[abs(pure_literal)] = pure_literal > 0
+
+            clauses = [clause for clause in clauses if pure_literal not in clause]
+
+            changed = True  # Since we modified the formula, check for more pure literals
+
+        return clauses, assignment
+
+    # ---------------------------------Solver-----------------------
+    def solve(self):
+        """
+        Perfroms the DPLL algorithm on a list of clauses (lists)
+        returns True is satisfiable, False if otherwise
+        """
+        clauses = self.clauses
+        assignments = {}
+
+        self.satisfiable = self.recursive_solve(clauses, assignments)
+        self.solved = True
+
+
+    def recursive_solve(self, clauses:list, assignments:dict):  
+        """
+        Perfroms the DPLL algorithm on a list of clauses (lists)
+        returns True is satisfiable, False if otherwise
+        """
+        
+
+        # Unit progpagation
+        clauses, assignments = self.unit_propagate(clauses, assignments)
+        if not clauses and not assignments:
+            return False
+                        
+        # Pure Literal Elimination
+        clauses, assignments = self.pure_literal_elimination(clauses, assignments)
+
+        # Stopping Conditions
+        if not clauses:
+            # If clauses is empty return True
+            return True
+        
+        # Continue DPLL
+        literal = clauses[0][0] # Choose a literal 
+        
+        yes_literal_clauses = self.update_clauses(clauses, literal)
+        if yes_literal_clauses is None:
+            yes_bool = False
+        else:
+            yes_bool = self.recursive_solve(yes_literal_clauses, assignments)
+        
+        no_literal_clauses = self.update_clauses(clauses, literal)
+        if no_literal_clauses is None:
+            no_bool = False
+        else:
+            no_bool = self.recursive_solve(no_literal_clauses, assignments)
+
+
+        return yes_bool or no_bool
     
-    # Continue DPLL
-    l = clauses[0] # Choose a literal 
-    # Return DPLL(clauses and +l) or DPLL(clause and -1)
+    # ---------------prints------------------
+    def results(self):
+        if self.solved:
+            return self.satisfiable
+        else:
+            raise RuntimeError("Solver not activated yet")
+
 
 if __name__ == "__main__":
 
@@ -42,4 +155,6 @@ if __name__ == "__main__":
     [2]           # (x2)
     ]
 
-    print(DPLL(clauses))
+    model = DPLL(clauses)
+    model.solve()
+    print(model.results())
